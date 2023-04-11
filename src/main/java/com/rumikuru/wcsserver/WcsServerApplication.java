b18929dba00f1241e5e758ad398d5075acfdb2cb
iliyaOrlov <bkmz.or32@gmail.com>
2023-04-11 20:16:44 +0300

    init comment

---
+++ src/main/java/com/rumikuru/wcsserver/WcsServerApplication.java
@@ -0,0 +1,13 @@
+package com.rumikuru.wcsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rumikuru/wcsserver/WcsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumikuru.wcsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
